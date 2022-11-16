418f4e75640fb8955e69e53a1792f6be6967909f
Loic Thieffry <thieffry.loic@gmail.com>
2022-11-16 16:59:38 +0000

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+puts "Creating restaurants.."
+5.times do
+  ["chinese", "italian", "japanese", "french", "belgian"]
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::Number.leading_zero_number(digits: 10),
+    category: ["chinese", "italian", "japanese", "french", "belgian"].shuffle.first
+  )
+  puts "Restaurant #{restaurant.name}"
+end
